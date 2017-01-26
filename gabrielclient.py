3fcc7268bc3cdbb032eb54120e3bd51f45f29d3e
Jamesjue <junjuew@cs.cmu.edu>
2017-01-26 17:23:23 -0500

    add in client side control of image resolution

--- gabrielclient.py
+++ gabrielclient.py
@@ -69,7 +69,11 @@ class VideoStreamingThread(SocketClientThread):
                 print 'image blurry. skip'
                 time.sleep(0.015)
                 ret, frame = video_capture.read()                
-            ret, jpeg_frame=cv2.imencode('.jpg', frame)
+            h, w, _ = frame.shape
+            if w > Config.MAX_IMAGE_WIDTH:
+                ratio = float(Config.MAX_IMAGE_WIDTH)/w
+                resized_frame = cv2.resize(frame, (0,0), fx = ratio, fy = ratio)
+            ret, jpeg_frame=cv2.imencode('.jpg', resized_frame)
             header={protocol.Protocol_client.JSON_KEY_FRAME_ID : str(id)}
             self._flag(header)
             header_json=json.dumps(header)
